9486212de2e9ac29fcc09480acf64384c701fe13
akihisa <s1520640@u.tsukuba.ac.jp>
2015-10-23 15:17:22 +0900

    Create Pager

--- app/Controller/PresentationsController.php
+++ app/Controller/PresentationsController.php
@@ -1,8 +1,8 @@
 <?php
 ini_set('auto_detect_line_endings', true);
 class PresentationsController extends AppController {
-	public $helpers = array('Html', 'Form', 'Text', 'Paginator');
-	public $components = array('Paginator');
+	public $helpers = array('Html', 'Form', 'Text');
+	
 	
 	public function index(){
 		$this->set('presentations', $this->Presentation->find('all'));
