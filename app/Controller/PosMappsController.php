615d5687efbd9bc492daab3f0e858f4ce4f47006
tsss-t <s1520728@u.tsukuba.ac.jp>
2015-10-28 15:44:41 +0900

    QR code生成とメイル発送のアクション追加

--- app/Controller/PosMappsController.php
+++ app/Controller/PosMappsController.php
@@ -18,12 +18,20 @@ class PosMappsController extends AppController {
     {
         $result=$this->Poster->find('all');
         $this->set('posters', $result);
-
     }
     public function qr($id)
     {
         $this->set("id",$id);
     }
-
+    public function  sendmail()
+    {
+            $content= Router::url('/PosMapps/index/', true);
+            App::uses('CakeEmail','Network/Email');
+            $Email = new CakeEmail('gmail');
+            $Email->from(array('tkb.tsss@gmail.com' => 'POSTOM'))
+                ->to('tkb.tsss@gmail.com')
+                ->subject('PosMapp URL')
+                ->send('<a href="'.$content.'"/>');
+    }
 }
 ?>
