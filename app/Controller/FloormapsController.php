61853af4ae823fe770ef855db74512fd11130583
tsss-t <s1520728@u.tsukuba.ac.jp>
2015-11-25 14:35:20 +0900

    select floormaps

--- app/Controller/FloormapsController.php
+++ app/Controller/FloormapsController.php
@@ -2,9 +2,40 @@
 class FloormapsController extends AppController {
 
     public $helpers = array('Html', 'Form', 'Text');
+    public $uses = array('Event');
     public function index(){
-
+        $this->Event->id = $_SESSION['event_id'];
+        $this->set('event', $this->Event->read());
     }
+    public function setmap()
+    {
+        $content= Router::url('/floormaps', true);
+        $this->Event->id = $_SESSION['event_id'];
+        $eventOld=$this->Event->read();
+        $test="";
+        if($eventOld['Event']['set_floormap']==true)
+        {
+            $eventNew=array('Event'=>array('id'=>$_SESSION['event_id'],'set_floormap'=>false));
+            $fields=array('set_floormap');
+            $this->Event->save($eventNew,false,$fields);
+
+            $this->Event->id = $_SESSION['event_id'];
+            $eventNew=$this->Event->read();
+            $test="this is true   ".$eventNew['Event']['set_floormap'];
+        }
+        else if($eventOld['Event']['set_floormap']==false) {
+            $eventNew=array('Event'=>array('id'=>$_SESSION['event_id'],'set_floormap'=>true));
+            $fields=array('set_floormap');
+            $this->Event->save($eventNew,false,$fields);
+
+            $this->Event->id = $_SESSION['event_id'];
+            $eventNew=$this->Event->read();
+            $test="this is false   ".$eventNew['Event']['set_floormap'];
+        }
+        $this->set('message',$eventOld['Event']['set_floormap']."        ".$test);
+        header("Location: ".$content);
+    }
+
     public function upload()
     {
 
@@ -36,6 +67,9 @@ class FloormapsController extends AppController {
                 move_uploaded_file($_FILES["file"]["tmp_name"],
                     $this->Html->webroot  ."floormap/".$_SESSION["event_id"].".". pathinfo($_FILES["file"]["name"], PATHINFO_EXTENSION));
                 $this->set("message", $_FILES["file"]["name"] . " upload success!");
+                $event=array('Event'=>array('id'=>$_SESSION['event_id'],'set_floormap'=>true));
+                $fields=array('set_floormap');
+                $this->Event->save($event,false,$fields);
             }
         }
         else
